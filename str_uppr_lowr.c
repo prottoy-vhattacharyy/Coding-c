867e52d1ae464af7ac62c5f6bc18eb3dc8d17259
prottoy-vhattacharyy <114688743+prottoy-vhattacharyy@users.noreply.github.com>
2023-10-14 10:45:41 +0600

    Update str_uppr_lowr.c

--- str_uppr_lowr.c
+++ str_uppr_lowr.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include<limits.h>
 
 int main() {
-    char a[1000];
+    unsigned long long x=ULLONG_MAX;
+    char a[x];
     gets(a);
     //for(int i=0 ;a[i]!='\0';i++) printf("%d ",a[i]);
     for(int i=0;a[i]!='\0';i++) {
